643a7234e44cc5cc0d4fafb4cb7fadca869b3e32
Jaime Castillo <jaime.castillo@softtek.com>
2026-03-23 11:18:40 +0100

    Creado el json

--- raterEngine/src/main/java/com/helloworld/callshop/engine/JSONParameterReader.java
+++ raterEngine/src/main/java/com/helloworld/callshop/engine/JSONParameterReader.java
@@ -1,4 +1,10 @@
 package com.helloworld.callshop.engine;
 
 public class JSONParameterReader {
+
+    //1. Acceder al fichero JSON.
+    //2. Parsearlo (Gson/Jackson).
+    //3. Extraer los parámetros de una tarifa concreta.
+    //4. Devolver un objeto ParametersMapper que la factory pueda usar.
+
 }
