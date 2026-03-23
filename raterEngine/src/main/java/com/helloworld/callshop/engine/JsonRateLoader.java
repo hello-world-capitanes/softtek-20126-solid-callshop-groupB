a284d520700c48a5e8fed2413a5875f02c3cb819
SOFTTEKPROJECTS\santiago.lopezj <santiago.lopezj@softtek.com>
2026-03-23 11:50:42 +0100

    Añado comentarios con las indicaciones que tenemos que realizar en la clase

--- raterEngine/src/main/java/com/helloworld/callshop/engine/JsonRateLoader.java
+++ raterEngine/src/main/java/com/helloworld/callshop/engine/JsonRateLoader.java
@@ -1,4 +1,8 @@
 package com.helloworld.callshop.engine;
 
 public class JsonRateLoader {
+    // Lee el fichero JSON completo y obtiene la lista de tarifas.
+    // Para cada tarifa obtiene el tipo y crea un ParametersMapper con sus parámetros.
+    // Localiza la RateFactory correspondiente y construye la tarifa.
+    // Registra cada tarifa en RatesRepository para que el motor pueda usarla.
 }
