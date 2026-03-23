59e5de379f6ce362df683e099acebf94da99f2b4
Jaime Castillo <jaime.castillo@softtek.com>
2026-03-23 13:32:36 +0100

    JSONParameterReader terminada pero no probada

--- raterEngine/src/main/java/com/helloworld/callshop/engine/JSONParameterReader.java
+++ raterEngine/src/main/java/com/helloworld/callshop/engine/JSONParameterReader.java
@@ -3,6 +3,7 @@ package com.helloworld.callshop.engine;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.helloworld.callshop.model.JSONParametersReader;
+import com.helloworld.callshop.model.RateConfig;
 import com.helloworld.callshop.model.RatesJson;
 import com.helloworld.callshop.rater.rate.factory.Parameter;
 import com.helloworld.callshop.rater.rate.factory.ParametersMapper;
@@ -12,30 +13,52 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
-    //1. Acceder al fichero JSON.
-    //2. Parsearlo (Gson/Jackson).
-    //3. Extraer los parámetros de una tarifa concreta.
-    //4. Devolver un objeto ParametersMapper que la factory pueda usar.
+//1. Acceder al fichero JSON.
+//2. Parsearlo (Gson/Jackson).
+//3. Extraer los parámetros de una tarifa concreta.
+//4. Devolver un objeto ParametersMapper que la factory pueda usar.
 
 public class JSONParameterReader implements JSONParametersReader {
 
     public RatesJson readJSONFile() throws IOException {
         //Evito tener la ruta del json hardcodeada
+
         ObjectMapper mapper = new ObjectMapper();
+
         try (InputStream is = getClass().getClassLoader().getResourceAsStream("rates.json")) {
+
             if (is == null) {
                 throw new IOException("No se ha encontrado el archivo rates.json");
             }
+
             return mapper.readValue(is, RatesJson.class);
         }
 
     }
 
-
     @Override
     public ParametersMapper readParameters(RatesJson parameters) {
-        return null;
+
+        ParametersMapperImpl paramsMapper = new ParametersMapperImpl();
+
+        for (RateConfig rateConfig : parameters.getTarifas()) {
+
+            Map<String, Object> params = rateConfig.getParametros();
+            if (params != null) {
+
+                for (Map.Entry<String, Object> entry : params.entrySet()) {
+
+                    String key = entry.getKey();
+                    Object value = entry.getValue();
+                    paramsMapper.put(key, value);
+
+                }
+            }
+        }
+
+        return paramsMapper;
     }
 
 
