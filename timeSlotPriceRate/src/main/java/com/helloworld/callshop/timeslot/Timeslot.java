389f2c4cf7444d9787b71119ae5734783faafb1e
SOFTTEKPROJECTS\alejandro.fernandezi <alejandro.fernandezi@softtek.com>
2026-03-23 13:19:39 +0100

    Fix Timeslot

--- timeSlotPriceRate/src/main/java/com/helloworld/callshop/timeslot/Timeslot.java
+++ timeSlotPriceRate/src/main/java/com/helloworld/callshop/timeslot/Timeslot.java
@@ -10,15 +10,10 @@ LocalTime startTime;
 LocalTime endTime;
 
     public Timeslot(LocalTime startTime, LocalTime endTime) {
-        this.startTime = startTime;
-        this.endTime = endTime;
+
 
         if (startTime == null || endTime == null) {
             throw new IllegalArgumentException("El tiempo de entrada o salida no puedeb ser nulos");
-        }
-
-        if (startTime.isAfter(endTime)) {
-            throw new IllegalArgumentException("El tiempo de entrada no puede ser después del tiempo de salida");
         }
 
         long duracion = duracionvalida(startTime,endTime);
@@ -28,23 +23,23 @@ LocalTime endTime;
                     "Duracion actual: " + duracion);
         }
 
+        this.startTime = startTime;
+        this.endTime = endTime;
 
     }
 
     private long duracionvalida(LocalTime startTime, LocalTime endTime) {
 
 
+        int startMin = startTime.toSecondOfDay() / 60;
+        int endMin = endTime.toSecondOfDay() / 60;
 
-        if (startTime.isBefore(endTime)) {
-            return Duration.between(startTime, endTime).toMinutes();
+      
+        if (endMin <= startMin) {
+            endMin += 24 * 60;
         }
 
-
-        Duration toMidnight  = Duration.between(startTime, LocalTime.MIDNIGHT);
-        Duration fromMidnight = Duration.between(LocalTime.MIN, endTime);
-
-        return toMidnight.plus(fromMidnight).toMinutes();
-
+        return endMin - startMin;
 
     }
 
