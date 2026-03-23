589133b95d8735369ec717f81653a86163e2bf43
Jaime Castillo <jaime.castillo@softtek.com>
2026-03-23 13:04:48 +0100

    Terminado el metodo ReadJSONFile y hecha la interfaz JSONParameterReader

--- raterEngine/src/main/java/com/helloworld/callshop/model/JSONParametersReader.java
+++ raterEngine/src/main/java/com/helloworld/callshop/model/JSONParametersReader.java
@@ -1,4 +1,12 @@
 package com.helloworld.callshop.model;
 
+import com.helloworld.callshop.rater.rate.factory.Parameter;
+import com.helloworld.callshop.rater.rate.factory.ParametersMapper;
+
+import java.util.List;
+
 public interface JSONParametersReader {
+
+    ParametersMapper readParameters(RatesJson parameters);
+
 }
